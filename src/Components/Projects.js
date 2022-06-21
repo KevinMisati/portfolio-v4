56f020e4ecd729cd7d090c252f4f0940dbe3ef37
kevinmisati <kevinmosigisi2001@gmail.com>
2022-06-21 16:41:59 +0300

    re-arranged projcts

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,36 +2,42 @@ import React from 'react'
 import "./Projects.css"
 import {Link} from "react-router-dom"
 import { ANIMATIONTYPE } from '../Pages/Home'
+import electronics from "../images/electronics.png"
+import liviti  from "../images/livit.png"
+import jumia from "../images/jumia.png"
+import construction from "../images/construction.png"
+import snapshot from "../images/snapshot.png"
+import nasa  from "../images/nasa.png"
 
 export const projects = [
     {
         title:"Electronics Shop",
         url: "https://lapstoro.netlify.app/",
-        img: "./images/electronics.png",
+        img: electronics,
         techs: [],
         desc:""
     },
     {
         title: "Hospitality Website",
         url: "https://liviti.netlify.app/",
-        img: "./images/livit.png"
+        img: liviti
     },
     {
         title: "Jumia",
         url: "https://jumia-clone1.netlify.app/",
-        img: "./images/jumia.png",
+        img: jumia,
         techs: [],
         desc: ""
     },
     {
         title: "Construction company",
         url: "https://we-build1.netlify.app/",
-        img: "./images/construction.png"
+        img: construction
     },
     {
         title: "Snap Shots",
         url: "https://photos01.netlify.app/",
-        img: "./images/snapshot.png",
+        img: snapshot,
         techs: [],
         desc: ""
     },
@@ -39,7 +45,7 @@ export const projects = [
     {
         title: "Nasa api",
         url: "https://nasaapii.netlify.app/",
-        img: "./images/nasa.png",
+        img: nasa,
         techs: [],
         desc:""
      },
