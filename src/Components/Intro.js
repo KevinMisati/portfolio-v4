21caa8020e953d069740a549ecd80e0ba8dc20b1
KevinMisati <kevinmosigisi2001@gmail.com>
2024-12-11 18:29:11 +0300

    improving the hero section

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import "./Intro.css"
+import coderImg from "../images/developer.png"
 
 const Intro = () => {
 
     return (
-    <div className="intro-container">
-        <section  id="home" className="intro" >
-            <p id="home" className="section_title_intro">
-                Hello, I'm Kevin <span className='waving-hand'>👋</span>
-            </p>
-            <h3 className="front-end-title">I build cool stuff for the web</h3>
-            
-            <a href="#work" className={`btn`}>
-                <i className="fas fa-eye"></i>
-                View my work 
-            </a>
+        <section className="intro-container">
+            <div  id="home" className="intro" >
+                <p id="home" className="section_title_intro">
+                    Hello, I'm Kevin <span className='waving-hand'>👋</span>
+                </p>
+                <h3 className="front-end-title">I build cool stuff for the web</h3>
+                
+                <a href="#work" className={`btn`}>
+                    <i className="fas fa-eye"></i>
+                    View my work 
+                </a>
+            </div>
+            <div className="img-container">
+                <img 
+                    src={coderImg}
+                    alt="Hero image"
+                />
+            </div>
         </section>
-    </div>
 )
 }
 
